9473df4c8636e3ae6f851e3d7117c3d95930a126
avinashrahul.gudimet <avinashrahul.gudimet@dish.com>
2017-07-12 16:05:01 -0600

    Installed Youtube Api search and react dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 // Import react
-// Go find the 'react' library from node_modules and assign it to React variable so this file can use it. 
-import React from 'react'
+// Go find the 'react' library from node_modules and assign it to React variable so this file can use it.
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+// Youtube api key
+const API_KEY = 'AIzaSyCOikGitYBBuKqY9jVbV81sggypognwvV4';
 //Create a Component
 
 //const is ES6 type and we cannot reassign any value to variable App
 
+// This is ES6 syntax
+// const App = () => {
+//   return <div>Hello Sling!!</div>;
+// }
+
 const App = function() {
-  return <div>Helo</div>;
+  return <div>Hello Sling!!</div>;
 }
 
 //Render a Component
+// ReactDOM.render(App) - Do not pass App directly since it is a class, create an instance like below
+ReactDOM.render(<App />, document.querySelector('.container'));
