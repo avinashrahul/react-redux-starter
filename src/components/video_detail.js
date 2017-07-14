34cf9e4a646441e49b81528623513efa38bf5788
avinashrahul.gudimet <avinashrahul.gudimet@dish.com>
2017-07-14 10:43:20 -0600

    Video Details component with ES6

---
+++ src/components/video_detail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+  const videoId = video.id.videoId;
+  // const videoUrl = 'https://www.youtube.com/embed/' + videoId;
+  const videoUrl = `https://www.youtube.com/embed/${videoId}`; // is same as above line whish is ES6 way
+  return (
+      <div className="video-detail col-md-8">
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe className="embed-responsive-item" src={videoUrl} />
+        </div>
+        <div className="details">
+          <div>{video.snippet.title}</div>
+          <div>{video.snippet.description}</div>
+        </div>
+      </div>
+  );
+};
+
+export default VideoDetail;
--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 // For a functional component props will be as argument
 // for class component props can be anywhere by this.props
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
   return(
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 }
